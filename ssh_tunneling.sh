28848a7db1e6826e6e9632978377be03eb9ff47a
Marcos Paulo <marcospaulo.mpa@gmail.com>
2022-09-19 18:32:17 -0300

    :memo:	modified:   ssh_tunneling.sh

--- ssh_tunneling.sh
+++ ssh_tunneling.sh
@@ -14,16 +14,16 @@ PID=$$
 
 list_services() {
   tag_no_figlet "${1:-"Checking active services - Current PID: $PID"}"
-  ps aux | grep -v grep | grep --perl-regexp "ssh -R|ssh_t" | awk '{print" "$2"\t"$11,$12,$13,$14,$15,$16,$17,$18;}'
+  ps aux | grep -v grep | grep --perl-regexp "ssh -o|ssh -R|ssh_t" | awk '{print" "$2"\t"$11,$12,$13,$14,$15,$16,$17,$18;}'
 }
 
 list_services_to_be_finished() {
   tag_no_figlet "List services to be finished"
-  ps aux | grep -v grep | grep --perl-regexp "ssh -R|ssh_t" | awk '{print" "$2"\t"$11,$12,$13,$14,$15,$16,$17,$18;}' | grep -v $PID
+  ps aux | grep -v grep | grep --perl-regexp "ssh -o|ssh -R|ssh_t" | awk '{print" "$2"\t"$11,$12,$13,$14,$15,$16,$17,$18;}' | grep -v $PID
 }
 
 terminate_old_services() {
-  ps aux | grep -v grep | grep --perl-regexp "ssh -R|ssh_t" | awk '{print $2}' | grep -v $PID | xargs --no-run-if-empty kill -9
+  ps aux | grep -v grep | grep --perl-regexp "ssh -o|ssh -R|ssh_t" | awk '{print $2}' | grep -v $PID | xargs --no-run-if-empty kill -9
 }
 
 terminate_ssh_connection() {
@@ -35,7 +35,25 @@ testing_connection() {
 }
 
 new_ssh_connection() {
-  ssh -R $port_remote_server:$ip_host_local:$port_host_local -N -f root@$ip_remote_server -p $port_ssh_server
+  ssh_return=$(ssh -R $port_remote_server:$ip_host_local:$port_host_local -N -f -o ConnectTimeout=$connect_timeout root@$ip_remote_server -p $port_ssh_server 2>&1)
+}
+
+loadding() {
+  array=("|" "/" "-" "\\")
+  i=0
+
+  PRINT="\033[1D OK\n"
+  while [[ "$nc_return" =~ .*(failed: Connection refused)$ ]]; do
+    sleep 0.2
+    testing_connection
+    printf "\033[1D${array[$i]}"
+    if [ $i -lt 3 ]; then
+      i=$(($i + 1))
+    else
+      i=0
+    fi
+  done
+  printf "$PRINT"
 }
 
 start_service() {
@@ -46,7 +64,7 @@ start_service() {
 
     if [[ "$nc_return" =~ .*( succeeded!)$ ]]; then
       date_log "Server" "$nc_return" "$VERDE"
-      sleep $time_sleep
+      sleep $time_for_waiting
 
     else
       date_log "Server" "$nc_return" "$VERMELHO"
@@ -54,25 +72,15 @@ start_service() {
       date_log "Server" "Terminate ssh service connections" "$VERMELHO" "$AMARELO"
       terminate_ssh_connection
 
-      date_log "Server" "Establishing new ssh service connections" "$VERMELHO" "$AMARELO"
+      date_log "Server" "Establishing new ssh service connection" "$VERMELHO" "$AMARELO"
       new_ssh_connection
 
-      date_log "Server" "Waiting for connection... " "$VERMELHO" "$AMARELO" "nom-break"
-      array=("|" "/" "-" "\\")
-      i=0
-
-      while [[ "$nc_return" =~ .*(failed: Connection refused)$ ]]; do
-        sleep 0.2
-        testing_connection
-        printf "\033[1D${array[$i]}"
-        if [ $i -lt 3 ]; then
-          i=$(($i + 1))
-        else
-          i=0
-        fi
-      done
-      printf "\033[1D OK\n"
-
+      if [ -n "$ssh_return" ]; then
+        date_log "Server" "$ssh_return" "$VERMELHO"
+      else
+        date_log "Server" "Waiting for connection...  " "$VERMELHO" "$AMARELO" "nom-break"
+        loadding
+      fi
     fi
 
   done

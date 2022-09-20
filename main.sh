4028ac05e0f3a21bd111802280a18cc5a543d3a1
Marcos Paulo <marcospaulo.mpa@gmail.com>
2022-09-20 15:43:04 -0300

    :bug:	modified: main.sh fix bug when create log folder

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 OUTPUT="stdout_$(date +%Y%m%d%H%M%S).txt"
 quantity_log=$(ls -a | grep ".txt" | wc -w)
+mkdir -p log
 if [ $quantity_log -gt 0 ]; then
   mv ./*.txt ./log/
 fi
-mkdir -p log
 ./ssh_tunneling.sh "$1" >$OUTPUT &

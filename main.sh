bdd351a2c496b1dab65c74f80769e11a42611813
Marcos Paulo <marcospaulo.mpa@gmail.com>
2022-09-20 11:42:18 -0300

    :bug:	modified: main.sh fix bug when creating the first log file

--- main.sh
+++ main.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 OUTPUT="stdout_$(date +%Y%m%d%H%M%S).txt"
-mv ./*.txt ./log/
+quantity_log=$(ls -a | grep ".txt" | wc -w)
+if [ $quantity_log -gt 0 ]; then
+  mv ./*.txt ./log/
+fi
 mkdir -p log
 ./ssh_tunneling.sh "$1" >$OUTPUT &
